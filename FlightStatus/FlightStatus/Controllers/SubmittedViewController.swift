b0231ea18ab0f0d8189227acacf3a8931ee1553f
Elaina <elainateramoto@gmail.com>
2018-07-31 23:16:56 -0700

    parsing json in real time works

---
+++ FlightStatus/FlightStatus/Controllers/SubmittedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SubmittedViewController.swift
+//  FlightStatus
+//
+//  Created by Elaina Teramoto on 7/31/18.
+//  Copyright © 2018 Elaina Teramoto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class SubmittedViewController: UIViewController{
+    override func viewDidLoad() {
+        print("This is the home screen view controller")
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
